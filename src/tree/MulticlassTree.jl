152f008e3a76449bdf95783d85ba94479bfaced1
HakauB <ballardhakau@gmail.com>
2022-05-06 22:28:52 +1200

    Single tree multi-class greedy forest implemented.

--- src/tree/MulticlassTree.jl
+++ src/tree/MulticlassTree.jl
@@ -417,6 +417,84 @@ function train_minimized_sampled(x::AbstractMatrix{Int}, y::AbstractMatrix{Int},
     return root
 end
 
+function train_minimized_greedy_forest(x::AbstractMatrix{Int}, y::AbstractMatrix{Int}, training_params::TrainingParameters) where T <: AbstractFloat
+    params = training_params.hyper_params
+    bin_sizes = training_params.histogram.num_bins
+
+    predictions, gradients, hessians = initialize_pgh(y, params)
+    gradients .= params.grad_func(y, predictions) .* params.case_weights
+    hessians .= params.hess_func(y, predictions) .* params.case_weights
+
+    root_weight = 0.0
+    root_gain = 0.0
+    root_y_i = 0
+    for y_i in 1:params.num_classes
+        grad_sum = vec_sum(gradients, y_i, collect(1:size(x, 1)))
+        hess_sum = vec_sum(hessians, y_i, collect(1:size(x, 1)))
+        weight = calculate_weight(grad_sum, hess_sum, params)
+        gain = calculate_gain(grad_sum, hess_sum, params)
+        if gain > root_gain
+            root_weight = weight
+            root_gain = gain
+            root_y_i = y_i
+        end
+    end
+    root = PredictionNode(1, root_weight, root_gain, collect(1:size(x, 1)), Vector{SplitterNode}(), root_y_i)
+    predictions[:, root_y_i] .+= root_weight .* params.learning_rate
+    gradients .= params.grad_func(y, predictions) .* params.case_weights
+    hessians .= params.hess_func(y, predictions) .* params.case_weights
+
+    stack = Vector{AbstractPredictionNode}()
+    push!(stack, root)
+
+    for iter in 1:params.iterations
+        best_node_index = 0
+        best_gain = 0.0
+        best_split_index = 0
+        best_split_value = 0
+        best_y_i = 0
+        for i in 1:size(stack, 1)
+            node = stack[i]
+            for y_i in 1:params.num_classes
+                gain, split_index, split_value = bin_split(x, gradients, hessians, y_i, node.indices, bin_sizes, params)
+                if gain > best_gain
+                    best_node_index = i
+                    best_gain = gain
+                    best_split_index = split_index
+                    best_split_value = split_value
+                    best_y_i = y_i
+                end
+            end
+        end
+        best_node = popat!(stack, best_node_index)
+
+        left_inds, right_inds = split_dataset(x, best_split_index, best_split_value, best_node.indices)
+
+        left_g, left_h = vec_sum(gradients, best_y_i, left_inds), vec_sum(hessians, best_y_i, left_inds)
+        right_g, right_h = vec_sum(gradients, best_y_i, right_inds), vec_sum(hessians, best_y_i, right_inds)
+
+        left_weight = calculate_weight(left_g, left_h, params)
+        right_weight = calculate_weight(right_g, right_h, params)
+        left_gain = calculate_gain(left_g, left_h, params)
+        right_gain = calculate_gain(right_g, right_h, params)
+
+        left_node = PredictionNode(best_node.id * 2, left_weight, left_gain, collect(left_inds), Vector{SplitterNode}(), best_y_i)
+        right_node = PredictionNode(best_node.id * 2 + 1, right_weight, right_gain, collect(right_inds), Vector{SplitterNode}(), best_y_i)
+        new_splitter = SplitterNode(best_split_index, best_split_value, left_node, right_node)
+        push!(best_node.splitter_nodes, new_splitter)
+        push!(stack, left_node)
+        push!(stack, right_node)
+
+        if best_node == root
+            pushfirst!(stack, best_node)
+        end
+
+        update_pgh!(y, left_weight, predictions, gradients, hessians, best_y_i, left_inds, params)
+        update_pgh!(y, right_weight, predictions, gradients, hessians, best_y_i, right_inds, params)
+    end
+    return root
+end
+
 function train(x::AbstractMatrix{T}, y::AbstractVector{Int}, params::HyperParameters; method::Symbol=:greedy) where T <: AbstractFloat
     x_binned, hist_info = histogram(x, params)
     training_params = TrainingParameters(params, hist_info)
@@ -435,6 +513,9 @@ function train(x::AbstractMatrix{T}, y::AbstractVector{Int}, params::HyperParame
         return Tree(root, training_params)
     elseif method == :minimizedsampled
         root = train_minimized_sampled(x_binned, y_hot, training_params)
+        return Tree(root, training_params)
+    elseif method == :minimizedgreedyforest
+        root = train_minimized_greedy_forest(x_binned, y_hot, training_params)
         return Tree(root, training_params)
     else
         error("Unknown method")
